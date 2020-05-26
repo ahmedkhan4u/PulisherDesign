be044937b45f3fa5605f32f3e3b955e7166ec7fb
Squadtech <wwwpink44@gmail.com>
2020-05-27 00:59:24 +0500

    publication list

--- app/src/main/java/com/softrasol/ahmed/pulisherdesign/Fragments/PublicationListFragment.java
+++ app/src/main/java/com/softrasol/ahmed/pulisherdesign/Fragments/PublicationListFragment.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.softrasol.ahmed.pulisherdesign.R;
@@ -20,6 +22,12 @@ import com.softrasol.ahmed.pulisherdesign.UploadPublicationActivity;
 public class PublicationListFragment extends Fragment {
 
     private FloatingActionButton floatingActionButton;
+    private String []
+            uploads = {"Uploads"},
+            stats = {"Stats"},
+            payouts = {"Payouts"};
+
+    private Spinner mSpinnerUploads, mSpinnerStats, mSpinnerPayouts;
 
     public PublicationListFragment() {
         // Required empty public constructor
@@ -30,8 +38,15 @@ public class PublicationListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_publication_list, container, false);
+
+
+        uploadsSpinner();
+        statsSpinner();
+        payoutsSpinner();
+
 
         floatingActionButton = mView.findViewById(R.id.fab);
 
@@ -43,6 +58,37 @@ public class PublicationListFragment extends Fragment {
         });
 
         return mView;
+    }
+
+    private void payoutsSpinner() {
+
+        mSpinnerPayouts = mView.findViewById(R.id.spinner_payout);
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(),
+                android.R.layout.simple_spinner_dropdown_item, payouts);
+
+        mSpinnerPayouts.setAdapter(adapter);
+
+    }
+
+    private void statsSpinner() {
+        mSpinnerStats = mView.findViewById(R.id.spinner_stats);
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(),
+                android.R.layout.simple_spinner_dropdown_item, stats);
+
+        mSpinnerStats.setAdapter(adapter);
+    }
+
+    private void uploadsSpinner() {
+
+        mSpinnerUploads = mView.findViewById(R.id.spinner_uploads);
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(),
+                android.R.layout.simple_spinner_dropdown_item, uploads);
+
+        mSpinnerUploads.setAdapter(adapter);
+
     }
 
 }
