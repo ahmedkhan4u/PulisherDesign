4b63dd06d7f2f32b5baecee888e99e40864b74e3
Squadtech <wwwpink44@gmail.com>
2020-05-27 00:17:04 +0500

    subscription prices

--- app/src/main/java/com/softrasol/ahmed/pulisherdesign/ChangeBusinessInfoActivity.java
+++ app/src/main/java/com/softrasol/ahmed/pulisherdesign/ChangeBusinessInfoActivity.java
@@ -1,11 +1,19 @@
 package com.softrasol.ahmed.pulisherdesign;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ChangeBusinessInfoActivity extends AppCompatActivity {
 
@@ -14,7 +22,7 @@ public class ChangeBusinessInfoActivity extends AppCompatActivity {
             typeOfContent = {"Newspapers", "Books", "Magazines"},
             frequency = { "daily", "biweekly", "weekly", "fortnightly",
                     "monthly", "quarterly", "annually" },
-            subcriptionPrices = {"1month", "2months", "3months", "6months", "1year"};
+            subcriptionPrices = {"Choose Price", "1month", "2months", "3months", "6months", "1year"};
 
 
     @Override
@@ -33,9 +41,58 @@ public class ChangeBusinessInfoActivity extends AppCompatActivity {
 
         mSpinnerSubscriptionPrices = findViewById(R.id.spinner_subscription_prices);
 
-        ArrayAdapter adapter = new ArrayAdapter(this,
+        final ArrayAdapter adapter = new ArrayAdapter(this,
                 android.R.layout.simple_spinner_dropdown_item, subcriptionPrices);
         mSpinnerSubscriptionPrices.setAdapter(adapter);
+
+
+        mSpinnerSubscriptionPrices.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, final int i, long l) {
+
+                if (subcriptionPrices[i] .equalsIgnoreCase("Choose Price")){
+                    return;
+                }
+
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(ChangeBusinessInfoActivity.this);
+                builder1.setMessage("Choose Price");
+                builder1.setCancelable(true);
+                View mView = LayoutInflater.from(ChangeBusinessInfoActivity.this)
+                        .inflate(R.layout.prices_dialog, null ,false);
+                builder1.setView(mView);
+
+                final TextInputEditText textInputEditText = mView.findViewById(R.id.choose_price);
+
+                builder1.setPositiveButton(
+                        "Save",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                String price = subcriptionPrices[i];
+                                subcriptionPrices[i] = price+" = "+textInputEditText.getText().toString().trim();
+                                adapter.notifyDataSetChanged();
+                                dialog.cancel();
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 
     private void frequencySpinner() {
--- app/src/main/java/com/softrasol/ahmed/pulisherdesign/UploadPublicationActivity.java
+++ app/src/main/java/com/softrasol/ahmed/pulisherdesign/UploadPublicationActivity.java
@@ -11,24 +11,11 @@ import java.lang.reflect.Array;
 public class UploadPublicationActivity extends AppCompatActivity {
 
     private Spinner mSpinner;
-    private String [] priceList = {"$10", "$20", "$30", "$40"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_publication);
 
-        //implementSpinner();
-
-    }
-
-    private void implementSpinner() {
-
-       // mSpinner = findViewById(R.id.spinner_price);
-
-        ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_spinner_dropdown_item, priceList);
-
-        mSpinner.setAdapter(adapter);
 
     }
 }
